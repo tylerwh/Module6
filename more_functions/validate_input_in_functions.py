a39c11aeb8a37a06a8e3d7fa61792b25ed999933
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-28 15:22:04 -0500

    Initial Commit for this file

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,19 @@
+"""
+Author: Tyler Hochstetler
+The purpose of this program is to practice passing arguments to functions.
+"""
+
+
+def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again!"):
+  """ This function takes the above parameters, validates them, then returns a concatenated test_name and test_score
+  :param test_name: required, name to be used
+  :param test_score: default is 0, but can take a number as an argument 
+  :param invalid_message: this displays if user inputs the incorrect number
+  :returns: the test_name and test_score concatenated together
+  """
+  # return {test_name: test_score}
+  pass
+
+
+if __name__ == "__main__":
+  pass
