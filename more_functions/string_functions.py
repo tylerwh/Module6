21a2dab56aabfcb35e8ea2200ab24d32e42aeaa0
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-28 07:42:52 -0500

    added content to multiply_string(); test passes

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,7 +1,13 @@
 """
-
+Author: Tyler Hochstetler
+This program is a practice for functions with parameters and returns
 """
 
 
 def multiply_string(message, n):
+  result = message * n
+  return result
+
+
+if __name__ == "__main__":
   pass
