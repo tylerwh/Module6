5c28676549696572d141585ee7e5e2c797a56d8c
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-28 07:48:37 -0500

    Added docstring with reST styling

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -5,6 +5,10 @@ This program is a practice for functions with parameters and returns
 
 
 def multiply_string(message, n):
+  """ This function takes a string, multiplies it, then returns the result
+  :param result: Takes the parameters that were passed into the function and multiplies them
+  :returns: the result parameter containing the multiplied string  
+  """
   result = message * n
   return result
 
